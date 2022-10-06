b75f2b61c0ec263515198c59ef2e97f1e407977e
halsted <lehalsted@ucdavis.edu>
2022-10-06 10:51:11 -0700

    Add files via upload

---
+++ filter.py
@@ -0,0 +1,130 @@
+# ---------------------------------------------------------------------
+# Project "Track 3D-Objects Over Time"
+# Copyright (C) 2020, Dr. Antje Muntzinger / Dr. Andreas Haja.
+#
+# Purpose of this file : Kalman filter class
+#
+# You should have received a copy of the Udacity license together with this program.
+#
+# https://www.udacity.com/course/self-driving-car-engineer-nanodegree--nd013
+# ----------------------------------------------------------------------
+#
+
+# imports
+import numpy as np
+
+# add project directory to python path to enable relative imports
+import os
+import sys
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+import misc.params as params 
+
+class Filter:
+    '''Kalman filter class'''
+    def __init__(self):
+        pass
+
+    def F(self):
+        ############
+        # TODO Step 1: implement and return system matrix F
+        ############
+      
+        dt = params.dt
+        return np.matrix([[1, 0, 0, dt, 0, 0],
+                          [0, 1, 0, 0, dt, 0],
+                          [0, 0, 1, 0, 0, dt],
+                          [0, 0, 0, 1, 0, 0],
+                          [0, 0, 0, 0, 1, 0],
+                          [0, 0, 0, 0, 0, 1]])
+        
+        ############
+        # END student code
+        ############ 
+
+    def Q(self):
+        ############
+        # TODO Step 1: implement and return process noise covariance Q
+        ############
+
+        dt = params.dt
+        q = params.q
+        a = (q*dt**3)/3
+        b = (q*dt**2)/2
+        c = q*dt
+        return np.matrix([[a, 0, 0, b, 0, 0],
+                          [0, a, 0, 0, b, 0],
+                          [0, 0, a, 0, 0, b],
+                          [b, 0, 0, c, 0, 0],
+                          [0, b, 0, 0, c, 0],
+                          [0, 0, b, 0, 0, c]])
+        
+        ############
+        # END student code
+        ############ 
+
+    def predict(self, track):
+        ############
+        # TODO Step 1: predict state x and estimation error covariance P to next timestep, save x and P in track
+        ############
+        
+        F = self.F()
+        x = F * track.x # state prediction
+        P = F * track.P * F.transpose() + self.Q() # covariance prediction
+        track.set_x(x)
+        track.set_P(P)
+        
+        ############
+        # END student code
+        ############ 
+
+    def update(self, track, meas):
+        ############
+        # TODO Step 1: update state x and covariance P with associated measurement, save x and P in track
+        ############
+
+        H = meas.sensor.get_H(track.x)
+        
+        # update state and covariance with associated measurement
+        S = self.S(track, meas, H)
+        gamma = self.gamma(track, meas)
+        
+        K = track.P * H.transpose() * np.linalg.inv(S) # Kalman gain
+        x = track.x + K * gamma # state update
+        I = np.matrix(np.identity(params.dim_state))
+        P = (I - K * H) * track.P # covariance update
+        track.set_x(x)
+        track.set_P(P)
+        ############
+        # END student code
+        ############ 
+        track.update_attributes(meas)
+    
+    def gamma(self, track, meas):
+        ############
+        # TODO Step 1: calculate and return residual gamma
+        ############
+        
+        hx = meas.sensor.get_hx(track.x)
+    
+        gamma = meas.z - hx # residual        
+        
+        return gamma
+        
+        ############
+        # END student code
+        ############ 
+
+    def S(self, track, meas, H):
+        ############
+        # TODO Step 1: calculate and return covariance of residual S
+        ############
+        
+        S = H * track.P * H.transpose() + meas.R # covariance of residual
+        
+        return S
+        
+        ############
+        # END student code
+        ############ 
